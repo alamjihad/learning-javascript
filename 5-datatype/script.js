b71c6038a63c509d952e82491a3b0970051ad26d
Jihad <bdjihad1862@gmail.com>
2023-11-09 13:49:59 +0600

    worked with datatype

---
+++ 5-datatype/script.js
@@ -0,0 +1,16 @@
+let x = 16 + 4 + " Volvo " + 16 + 4 ;
+console.log(x);
+
+let z = 16 + 4 + " Volvo";
+console.log(z);
+
+let y ="Volvo " + 16 + 4 ;
+console.log(y);
+
+let p=false;
+console.log(typeof p);
+
+let jihad = {name: "Nur-Alam",age: 20,hobby: "reading books",passion: "palying cricket",favouriteNumber: 98};
+
+console.log(jihad,typeof jihad);
+console.log(typeof jihad);
