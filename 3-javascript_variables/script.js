923b85906bc0b350e04f7c56af1b9ba7079d88b6
Jihad <bdjihad1862@gmail.com>
2023-11-09 12:48:56 +0600

    complete operator chapter

---
+++ 3-javascript_variables/script.js
@@ -0,0 +1,7 @@
+const a=5;
+console.log(a)
+
+const car=["Volvo","Sabb","BMW"];
+car[0]="Miami";
+car[1]="Ashpat";
+console.log(car);
---
+++ 4-operators-arithmatic_operation/main.js
@@ -0,0 +1,40 @@
+let s;
+s=10**10;
+console.log(s);
+
+
+let x=5;
+let y="High";
+let z=y+" "+x;
+console.log(z);
+
+
+let a=5;
+
+let b="5";
+
+let c=b+" "+a;
+
+let e=5;
+
+let d=e+a;
+
+let g=b+a;
+
+console.log(c);
+
+console.log(c);
+
+console.log(d);
+
+console.log(typeof g);
+
+console.log(typeof c);
+
+console.log(typeof d);
+
+console.log(typeof z);
+
+let z3=5;
+
+console.log(z3%=100);      
