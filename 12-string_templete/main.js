69dfb32b56c23a721da2878caf0160dc1fc6b538
Jihad <bdjihad1862@gmail.com>
2023-11-11 22:02:10 +0600

    completed string templete

---
+++ 12-string_templete/main.js
@@ -0,0 +1,7 @@
+let country =`Bangladeshis a beautiful country`;
+ console.log(country);
+
+ let a=17;
+ let b=1;
+ let sentence =`I love ${country} and it's population is ${a+b} crore.`;
+ console.log(sentence);
