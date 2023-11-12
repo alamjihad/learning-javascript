f71f98c19ce5ffddbca1d59a02fc014c7a8563aa
Jihad <bdjihad1862@gmail.com>
2023-11-12 12:46:32 +0600

    worked with number

---
+++ 13-number_dtatype/script.js
@@ -0,0 +1,17 @@
+let x=0.2+0.1;
+document.getElementById('demo').innerHTML="0.3 + 0.1 = " + x;
+
+let y=(0.2*10+0.1*10)/10;
+document.getElementById('dimo').innerHTML=y;
+
+var c=10;
+var s=20;
+//document.getElementById('pure').innerHTML = "The result is: "+ c+s;
+document.getElementById('pure').innerHTML =c+s+ " The result is: ";
+
+const a="nfweoriowejflenf";
+const d=NaN;
+var b=45;
+console.log(a/b);
+console.log(d/b);
+console.log(typeof (isNaN(a/b)));
