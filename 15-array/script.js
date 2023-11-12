0077a5f2c11a409926e4d7b77aed5b1f29a91b1e
Jihad <bdjihad1862@gmail.com>
2023-11-12 14:17:42 +0600

    complete half of array

---
+++ 15-array/script.js
@@ -0,0 +1,14 @@
+const cars =["Saab", "Volvo", "BMW"];
+//document.getElementById("demo").innerHTML = cars;
+console.log(typeof cars);
+console.log(cars.length);
+console.log(cars.sort);
+
+const fruits=["Banana","Orange","Apple","Mango"];
+let flen=fruits.length
+for(i=0; i<flen; ++i)
+{
+    console.log(i);
+    console.log(fruits[i]);
+}
+console.log("Finish");
