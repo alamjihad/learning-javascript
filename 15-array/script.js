513a7f52a58079520f76c8f27c605ffb75400ce0
Jihad <bdjihad1862@gmail.com>
2023-11-12 21:53:17 +0600

    complete array

--- 15-array/script.js
+++ 15-array/script.js
@@ -1,14 +1,33 @@
 const cars =["Saab", "Volvo", "BMW"];
-//document.getElementById("demo").innerHTML = cars;
+document.getElementById("demo").innerHTML = cars;
 console.log(typeof cars);
 console.log(cars.length);
 console.log(cars.sort);
 
 const fruits=["Banana","Orange","Apple","Mango"];
+fruits[fruits.length]="Guava";
+fruits[5]="Jackfruik";
+fruits.push("Lemon");
 let flen=fruits.length
 for(i=0; i<flen; ++i)
 {
     console.log(i);
     console.log(fruits[i]);
 }
-console.log("Finish");
+
+function myfunction(value){
+    console.log(value);
+}
+fruits.forEach(myfunction);
+
+// const person ={
+//     name: "Sabbir",
+//     age:11,
+//     country:"Banglaesh"
+// };
+const person = [];
+person["first-name"]="Jhon";
+person["last-name"]="Doe";
+person["age"]=46;
+person.length;
+person[0];
