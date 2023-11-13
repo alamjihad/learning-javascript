7f9813dd005115e8ecac4ec07aa8bdac08202724
Jihad <bdjihad1862@gmail.com>
2023-11-13 21:03:31 +0600

    complete for loop and loop scope

---
+++ 24-for_loop/script.js
@@ -0,0 +1,29 @@
+const car =["A","B","C","D","E"];
+for(let i=10,j=1;i>car.length;--i)
+{
+    console.log(i);
+    console.log(j);
+}
+
+let i=0,
+    j=1;
+    for(;;i=i+2)
+    {
+        if(i>10)
+        {
+            break;
+        }
+        else{
+            console.log(i);
+        }
+    }
+
+console.log("I am done");
+
+let k=5;
+for(let k=0;k<=10;++k)
+{
+    console.log(k);
+}
+console.log("this is "+k);
+console.log("this is",k);
