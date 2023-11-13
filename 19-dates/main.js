c09e79cd79d8cc42a9b24ccc12a3084b47114914
Jihad <bdjihad1862@gmail.com>
2023-11-13 14:15:53 +0600

    woked with date basics

---
+++ 19-dates/main.js
@@ -0,0 +1,12 @@
+const d = new Date();
+document.getElementById("da").innerHTML=(Date());
+console.log(Date());
+                                                                //javascript count  month from 0 to 11 as array
+                                                                //toDatestrinfg
+                                                                //toUTCstring
+                                                                //toISOstring
+
+const da = new Date(2018,11);
+console.log(da);
+const ya =new Date(2018);
+console.log(ya);
