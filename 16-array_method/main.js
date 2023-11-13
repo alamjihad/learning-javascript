8d3091333bbff0f3046b5c42d3828f227b6eaeba
Jihad <bdjihad1862@gmail.com>
2023-11-13 11:43:49 +0600

    complete array method

---
+++ 16-array_method/main.js
@@ -0,0 +1,17 @@
+// const car=["Car","cAr","caR"];                                       // push in array elem. in last
+// let a =car.unshift()                                                      // pop out array elem. in last
+// console.log(a);                                                            // splice in array elem. in middle
+                                                                              // unshift in array elem. in fast
+                                                                                // shift out array elem. in fast
+
+const girl=["Cecilie","Lone","Linus"];
+const boy=["Emil","Tobias","Linus"];
+const child=["one","two","three"]
+
+const children=girl.concat("ram","piu",boy,child,"sam","rom");
+document.getElementById("chil").innerHTML=girl;
+//console.log(children);
+
+/*const fruits=["Banana","Orange","Apple","Lemon","Mango"];
+const citrus = fruits.slice(3);        /const citrus = fruits.slice(1,3);
+console.log(ctirus);*/
