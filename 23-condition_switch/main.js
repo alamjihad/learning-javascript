0acabf5a08a607c94a609899e56f52de6b76b332
Jihad <bdjihad1862@gmail.com>
2023-11-13 20:24:56 +0600

    done condition and switch

---
+++ 23-condition_switch/main.js
@@ -0,0 +1,17 @@
+let age=18;
+if(age<18)
+{
+    console.log("You are young");
+}
+else if(age==18)
+{
+    console.log("Welcome adult");
+}
+else if(age>18&&age<30)
+{
+    console.log("You are matured");
+}
+else
+{
+    console.log("You are old");
+}
