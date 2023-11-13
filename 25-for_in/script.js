e6fc4cbe510e54a9411ccb7dfe6424b326db596f
Jihad <bdjihad1862@gmail.com>
2023-11-13 21:37:11 +0600

    complete for in

---
+++ 25-for_in/script.js
@@ -0,0 +1,9 @@
+const person ={ fname:"Jhon",lname:"Doe",age:25};
+for(let x in person){
+    console.log(person[x]);
+}
+
+const numbers=[45,4,9,16,25];
+for(let number in numbers){
+    console.log(numbers[number]);
+}
