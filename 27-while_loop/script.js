27b1461b56bd17be7fca4bfed5bb08a4dd31db78
Jihad <bdjihad1862@gmail.com>
2023-11-13 22:21:42 +0600

    worked with while

---
+++ 27-while_loop/script.js
@@ -0,0 +1,18 @@
+let i=0;
+let text = "";
+while(i<10)
+{
+    text+="The number is "+i;
+    ++i;    
+}
+console.log(text);
+
+
+let j=0;
+let text1 = "";
+do{
+    text1+="The number is "+j;
+    ++j;    
+}
+while(j<10)
+console.log(text1);
