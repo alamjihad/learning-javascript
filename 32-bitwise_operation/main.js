450209e70e06866b2e8dfa72e69a2b2e0cd47e48
Jihad <bdjihad1862@gmail.com>
2023-11-15 21:45:03 +0600

    worked with bitwise operaton

---
+++ 32-bitwise_operation/main.js
@@ -0,0 +1,15 @@
+console.log((3).toString());
+
+
+
+/*----------------------------decimal to binary-----------------*/
+
+
+console.log((3).toString(2));
+
+
+
+
+/*-----------------------------binary to decimal--------------------------*/
+
+console.log((0b101).toString(10));
