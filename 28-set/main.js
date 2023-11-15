a454aab8b14cd0cbaa11a40dc52cbcd748472eaf
Jihad <bdjihad1862@gmail.com>
2023-11-15 12:30:18 +0600

    worked with set

---
+++ 28-set/main.js
@@ -0,0 +1,36 @@
+const letter =new Set(["a","b","c"]);
+letter.add("d");
+letter.add("e");
+letter.add("d");
+letter.add("e");
+console.log(letter.values());
+
+
+let text2="";
+for(const x of letter.values())
+{
+    text2+=x;
+}
+console.log(text2);
+
+
+// <!--------------------------------------variable in ------------------------------------->
+
+const letters=new Set();
+const a="a";
+const b="b";
+const c="c";
+
+letters.add("a");
+letters.add("b");
+letters.add("c");
+letters.add("a");
+letters.add("b");
+//console.log(letters);
+let text ="";
+letters.forEach(function(value)
+{
+    text+=value;
+})
+console.log(text);
+
