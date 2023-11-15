f279a9883dabfd64ed699812a2e223abb67ade5c
Jihad <bdjihad1862@gmail.com>
2023-11-15 20:10:12 +0600

    worked with type conversion

---
+++ 31-type_conversion/script.js
@@ -0,0 +1,14 @@
+console.log(parseInt(3.2));
+console.log(typeof(String(100+23)));
+
+let a =10e6;
+console.log(a.toExponential());
+
+let b =10.5;
+console.log(b.toFixed());
+
+let c =10.378787;
+console.log(c.toPrecision(3));
+
+let d={name:"Fjohn"};
+console.log(typeof(d.toString()));
