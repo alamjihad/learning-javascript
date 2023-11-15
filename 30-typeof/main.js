340482456cdab53f9f121d5b6dc0a9f883ec0f00
Jihad <bdjihad1862@gmail.com>
2023-11-15 19:35:28 +0600

    type of complete

--- 30-typeof/main.js
+++ 30-typeof/main.js
@@ -5,3 +5,10 @@ let c=b+" "+a+" "+d;
 console.log(c);
 
 console.log(typeof(typeof {fname:'Bangladesh',age:50}));
+
+function isArray(myArray){
+    console.log(myArray.constructor.toString().indexOf('Array')>-1);
+}
+
+ isArray([1,2,3]);
+// isArray({fname:"Rizwan"});
