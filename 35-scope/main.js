e695d91c48e44fea9837eddda633010e19b291c9
Jihad <bdjihad1862@gmail.com>
2023-11-16 21:28:23 +0600

    worked with scoped

---
+++ 35-scope/main.js
@@ -0,0 +1,16 @@
+var carName1="Volvo";
+var carName2="Volvo";
+var carName3="Volvo";
+var carName4="Volvo";
+var carName5="Volvo";
+
+console.dir(window);
+
+
+let carName6="Saab";
+function A()
+{
+    console.log(window.carName6);
+}
+
+A();
