178478957896da0ebbc548b34720ce144e6a77c6
Jihad <bdjihad1862@gmail.com>
2023-11-19 11:10:14 +0600

    worked with arrow function

---
+++ 38-arrow_function/main.js
@@ -0,0 +1,10 @@
+// arrow function for making function shorter
+hello = () => {
+    return "Hello World";
+}
+
+// shortest one line function if only one line used return statement
+hello = () => "Hello World";
+
+// if there is one parameter you dont need to use bracket
+hello = val => "Hello " + val;
