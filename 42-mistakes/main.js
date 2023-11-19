7ec79fd59f31816af9719b8ccea1190f160606c8
Jihad <bdjihad1862@gmail.com>
2023-11-19 18:21:02 +0600

    worked with mistakes

---
+++ 42-mistakes/main.js
@@ -0,0 +1,16 @@
+var x=0.1;
+var y=0.2;
+console.log(x+y);
+
+var a=x*10;
+var b=y*10;
+console.log((a+b)/10);
+
+
+const numbers = [1,2,3];
+
+numbers['Polash']=0;
+
+console.log(numbers);
+
+console.log(numbers[0]);
