c47056ca26efb410d887be5c4cce1b0fdfb555ff
Jihad <bdjihad1862@gmail.com>
2023-11-21 20:38:21 +0600

    worked with object set

---
+++ 53-object_sets/script.js
@@ -0,0 +1,24 @@
+// const letters = new Set(["a","b","c"]);
+// const lettersIterator = letters.values();
+// for(let a of lettersIterator){
+//     console.log(a);
+// }
+//  console.log(letters.values());
+//  console.dir(letters);
+
+
+const a ={
+    firstName : 'Ali',
+    lastName : 'Akabbar'
+}
+console.log(Object.entries(a));
+
+//const letters = new Set(['a','b','c']);
+// const myIterator =letters.entries();
+// for(const entry of myIterator){
+//    // text+=entrty;
+//    console.log(entry);
+// }
+const letters =['a','b','c'];
+console.log(typeof letters);
+console.log(letters instanceof Set);
