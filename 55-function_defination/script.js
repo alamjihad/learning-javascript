cc7902e7e97aa76e73a0f79b3b665edb01a3fbe6
Jihad <bdjihad1862@gmail.com>
2023-11-21 23:31:47 +0600

    complete function defination

--- 55-function_defination/script.js
+++ 55-function_defination/script.js
@@ -1,5 +1,5 @@
 const a=function (){                                    // function expression can't be hoisted
-    console.log("I am sleeping");                       // function defination can be hoisted
+    console.log("I am sleeping");                       // function declaration can be hoisted
 };
 a();
 
@@ -9,8 +9,23 @@ a();
 })();
 
 
-function myFunction(a,b){
-    return a*b;
+function myFunction(a,b){    
+    console.log(arguments.length)
+    console.log(arguments);     // if a function don't return anything or print nothing its returned
+    return a*b;                                         // undefined to calling variable
 }
-const result =myFunction(4,5);
+const result =myFunction(4,5)*2;
 console.log(result);
+
+
+function my(){
+    console.log("Hello");
+}
+console.dir(my);
+
+
+const b= (x,y)=>x*y;             // syntax of arrow function we can use it when function take parameters and 
+                                 // return an opeartion.If there another line if code without input arguments
+                                 // and return it will not work here. .this keyword will not work here
+
+
