55729e2d844322cc4dddb247d7526b972218ed9a
Jihad <bdjihad1862@gmail.com>
2023-11-23 22:01:34 +0600

    complete changing css

---
+++ 71-change_css/script.js
@@ -0,0 +1,12 @@
+// const p=document.getElementById("demo")
+// setTimeout(function(){ 
+//     const p = document.querySelector('#demo');
+//     p.style.color='blue';
+//     p.style.fontSize="25px";}
+//     ,2000);
+
+function styling(){ 
+    const p = document.querySelector('#demo');
+    p.style.color='black';
+    p.style.fontSize="50px";
+};
