9c87e2352b96df8a00f6e91b3ebbb5e5d2ede5bd
Jihad <bdjihad1862@gmail.com>
2023-11-25 20:41:17 +0600

    worked with window object

---
+++ 78-window_object/main.js
@@ -0,0 +1,19 @@
+// select dom element
+let myWindow;
+
+const widht=document.getElementById('width');
+const height=document.getElementById('height');
+
+// show window object properties 
+
+widht.innerHTML ="Window inner width is: " + window.innerWidth;
+height.innerHTML ="Window inner hight is: " + window.innerHeight;
+
+
+function openWindow(){
+    myWindow=window.open("https://google.com","_self");
+}
+
+function closeWindow(){
+    myWindow.close();
+}
