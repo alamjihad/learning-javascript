87cffea300892ddfb2c003cd85e290e475738f00
Jihad <bdjihad1862@gmail.com>
2023-11-26 22:33:56 +0600

    ajax and xmlhttp request

--- 92-ajax_copy/script.js
+++ 92-ajax_copy/script.js
@@ -1,11 +1,23 @@
-function loadData(){
+function loadData(callbackF){
     const xhr = new XMLHttpRequest ();                                              // create a new request
-    xhr.onload = function(){                                                  // what to do when response arrives
+    xhr.onload = function(){
+        callbackF(this);
+    } /*function(){                                   // what to do when response arrives
         const container = document.getElementById("demo");
-        container.innerHTML = xhr.responseText;
-    };
+        demo.innerHTML = xhr.responseText;
+    };*/
     xhr.open("GET","./data.txt");                          // prepare request - methods: GET,POST,PUT,PATCH,DELETE
    // xhr.setRequestHeader("MY_GF","JavaScript");                                     // add a request header
     xhr.send();                                                                          // send a request
    // xhr.abort();          // abort request
+}
+
+function myCallback1(xhr){
+    const container = document.getElementById("demo");
+    container.innerHTML = xhr.responseText;
+}
+
+function myCallback2(xhr){
+    const container = document.getElementById("demo2");
+    container.innerHTML = xhr.responseText;
 }
