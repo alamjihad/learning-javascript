ae31823b2ac33120ac8697b3d9df3d07ee321757
Jihad <bdjihad1862@gmail.com>
2023-11-26 14:11:12 +0600

    worked js cookies

---
+++ 84-cookies/script.js
@@ -0,0 +1,36 @@
+function setCookie(cname,cvalue,exdays){
+    const d= Date();
+    d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+    let expires="expires=" +d.toUTCString();
+    document.cookie=cname + "=" +cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname){
+    console.log(document.cookie);
+    let ca=document.cookie.split(";");
+    for(let i=0;i<ca.length;i++)
+    {
+        let name=cname + "=";
+        let c = ca[i];
+        while(c.charAt(0)===" "){
+            c=c.substring(1);
+        }
+        if(c.indexOf(name)===0){
+            return c.substring(name.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie(cname,exdays){
+    let a = getCookie(cname);
+    if(a!=""){
+        alert("Welcome again " + a);
+    }
+    else{
+        name =prompt("Please enter your name:", "");
+        if(cname != "" && cname != null){
+            setCookie("cname",name,exdays);
+        }
+    }
+}
