cd4480d11a43839ab5e642f526598e2ae1f4f751
Jihad <bdjihad1862@gmail.com>
2023-11-27 16:00:37 +0600

    complete json stringify

---
+++ 99-stringify/script.js
@@ -0,0 +1,16 @@
+const obj ={name:"John",age:38,city:"Dhaka",today:new Date()};
+
+console.log(obj);
+
+const my= JSON.stringify(obj);
+
+console.log(my);
+
+
+const arr=["John","Anna","Peter"];
+
+console.log(arr);
+
+const ar= JSON.stringify(obj);
+
+console.log(ar);
