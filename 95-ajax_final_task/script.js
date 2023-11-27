0f6d8593b8c12c0fb296d88fefa07cde1e40a917
Jihad <bdjihad1862@gmail.com>
2023-11-27 11:30:02 +0600

    example of XMLhttp request get and send''

---
+++ 95-ajax_final_task/script.js
@@ -0,0 +1,48 @@
+function sendRequest(method,url,data){
+    const promise =new Promise((resolve,reject)=>{
+        const xhr = new XMLHttpRequest();
+        xhr.onload =function(){
+           if(this.status>=400)                                                     // handle application error
+           {
+            reject(`There was an application error and the status is ${this.status}`);
+           }
+         resolve(this.response);                                           // console.log(this.responseText);
+                                                                           // console.log(this.response);
+        };
+        xhr.onerror =function(){
+            reject("There was an error");
+           };
+        xhr.open(method,url);
+        xhr.responseType="json";
+        xhr.send(data);
+    });
+    return promise;
+}
+
+function getData(){
+   sendRequest("Get","https://jsonplaceholder.typicode.com/todos/1")
+   .then(responseData=>{
+    console.log(responseData);
+   })
+   .catch((err)=>{
+    console.log(err);
+   })
+}
+
+function sendData(){
+    sendRequest("POST","https://jsonplaceholder.typicode.com/posts",
+    JSON.stringify({
+        title: "foo",
+        body: "bar",
+        userId: 1,
+    })
+    ).then(responseData=>{
+     console.log(responseData);
+    });
+}
+
+const getButton=document.getElementById("get");
+//const sendButton=document.sendElementById("send");
+
+getButton.addEventListener("click",getData);
+//sendButton.addEventListener("click",sendData);
