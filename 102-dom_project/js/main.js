7187b81ac1b12998353dc66af29467468fcc40d8
Jihad <bdjihad1862@gmail.com>
2023-11-27 22:21:12 +0600

    worked on programming hero dom project

---
+++ 102-dom_project/js/main.js
@@ -0,0 +1,84 @@
+const milestonesData = JSON.parse(data).data;
+
+// load course milestone data
+function loadMilestones(){
+    const milestones = document.querySelector('.milestones');
+
+    milestones.innerHTML = `${milestonesData.map(function(milestone){
+        return `<div class="milestone border-b" id="${milestone_id}">
+                <div class="flex">
+                <div class="checkbox"><input type="checkbox" onclick="markMilestone(this,${milestone._id}) /></div>
+                <div onclick="openMilestone(this, ${milestone._id})">
+                    <p>
+                        ${milestone.name}
+                    <span><i class="fas fa-chevron-down"></i></span>
+                    </p>
+                </div>
+                </div>
+                <div class="hidden_panel">
+                    ${milestone.modules
+                        .map(function(module){
+                        return `<div class="module broder-b">
+                        <p>${module.name}</p>
+                        </div>`;
+                    }).join("")}
+                </div>
+            </div>`
+    }).join("")}`;
+}
+function openMilestone(milestoneElement, id){
+    const currentPanel = milestoneElement.parentNode.nextElementSibling;
+    contshownPanel = document.querySelector(".shown");
+    const active = document.querySelector(".active");
+    // removed previous active class if any [other than the clicked one]
+    if(active && milestoneElement.classList.contains("active")){
+        active.classList.remove("active");
+    }
+
+    // toggle current clicked one 
+    milestoneElement.classList.toggle("active");
+    // first hide previous panel if open [other than the clicked element] 
+    if(!currentPanel.classList.contains("show")&& shownPanel)
+    shownPanel.classList.remove("show");
+    // toggle current element 
+    currentPanel.classList.toggle('show');
+
+    showMilestone(id);
+}
+function showMilestone(id){
+    const milestoneImage =document.querySelector(".milestoneImage");
+    const name =document.querySelector(".title");
+    const details =document.querySelector(".details");
+
+    milestoneImage.computedStyleMap.opacity="0";
+    milestoneImage.src = milestonesData[id].image;
+    name.innerText = milestonesData[id].name;
+    details.innerText = milestonesData[id].description;
+}
+
+// listen for hero image load
+const milestoneImage = document.querySelector(".milestoneImage");
+milestoneImage.onload = function(){
+    this.computedStyleMap.opacity ="1";
+}
+
+function markMilestone(checkbox, id){
+    const doneList =document.querySelector(".doneList");
+    const milestonesList =document.querySelector(".milestones");
+    const item = document.getElementById(id);
+
+    if(checkbox.checked){
+        // mark as done
+        milestonesList.removeChild(item);
+        doneList.appendChild(item);
+    }else{
+        milestonesList.appendChild(item);
+        doneList.removeChild(item);
+        // back to main list
+    }
+}
+
+// overload or sorting
+// reload list
+
+loadMilestones();
