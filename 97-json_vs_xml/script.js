63474cbf165ac4b109337e94473931d078241bc5
Jihad <bdjihad1862@gmail.com>
2023-11-27 13:32:54 +0600

    difference between json and xml

---
+++ 97-json_vs_xml/script.js
@@ -0,0 +1,45 @@
+{"employes" ;[                                                                      // JSON example
+    {
+        "firstName":"John","lastName":"Doe"
+    },
+    {
+        "firstName":"Anna","lastName":"Smith"
+    },
+    {
+        "firstName":"Peter","lastName":"Jones"
+    }
+]}
+
+<employes>                                                                      //~ XML example
+
+    <employe>
+        <firstName>
+            John
+        </firstName>
+
+        <lastName>
+            Doe
+        </lastName>
+    </employe>
+
+    <employe>
+        <firstName>
+            Anna
+        </firstName>
+
+        <lastName>
+            Smith
+        </lastName>
+    </employe>
+
+    <employe>
+        <firstName>
+            Peter
+        </firstName>
+
+        <lastName>
+            Jones
+        </lastName>
+    </employe>
+
+</employes>
