076ff242b4120a55d37fe81d74fda472b6ca3572
Jihad <bdjihad1862@gmail.com>
2023-11-28 10:04:31 +0600

    modified in two files

--- 98-json.parse/script.js
+++ 98-json.parse/script.js
@@ -1,15 +1,15 @@
-const jsonString = '{"name":"John","age":30,"city":"Dhaka","birth":"18-06-2003"}'
+const jsonString = '{"name":"John","age":30,"city":"Dhaka","birth":"2003-06-18"}'
 console.log(jsonString);
 // convert to valid js object
- const myObj=JSON.parse(jsonString,/*function(key,value){
+ const myObj=JSON.parse(jsonString,function(key,value){
 //     // console.log(`${key}-${value}`);                                   // still now not working
 //     // console.log("----------");
-    if(key==="birth"){                                  
-        return new Date(value);
-    }
+    // if(key==="birth"){                                  
+    //     return new Date(value);
+    // }
     return value;
- }*/);
- //console.log(new Date(myObj.birth));                                            // new Date not working
+ });
+ console.log(new Date(myObj.birth));                                            // new Date not working
 console.log(myObj);
                            
 document.getElementById("demo").innerHTML=myObj.name+" "+myObj.age+" "+myObj.city;
